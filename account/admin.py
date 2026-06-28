51be35da0b26fa46f759d62d01a6e85915c917fc
ibrohim-programer <abduraximovibrohim821@gmail.com>
2026-06-28 21:30:36 +0500

    fix: hash password when creating or updating user via django admin

--- account/admin.py
+++ account/admin.py
@@ -8,6 +8,19 @@ User = get_user_model()
 class UserModelAdmin(admin.ModelAdmin):
     list_display = ["id","phone","full_name","role","avatar","is_active","is_staff"]
     
+    def save_model(self, request, obj, form, change):
+        # Hash password if it's set in plain text (e.g. on creation or when updated)
+        if not change:
+            obj.set_password(obj.password)
+        else:
+            try:
+                orig_obj = User.objects.get(pk=obj.pk)
+                if obj.password != orig_obj.password:
+                    obj.set_password(obj.password)
+            except User.DoesNotExist:
+                obj.set_password(obj.password)
+        super().save_model(request, obj, form, change)
+    
 
 @admin.register(Lavozim)
 class LavozimAdmin(admin.ModelAdmin):
