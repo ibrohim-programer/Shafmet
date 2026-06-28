c956925c5930c006a10c7fbdb661d1bcc553869a
ibrohim-programer <abduraximovibrohim821@gmail.com>
2026-06-28 23:26:40 +0500

    .

--- task_and_assessment/urls.py
+++ task_and_assessment/urls.py
@@ -11,13 +11,13 @@ from .views import (
 
 urlpatterns = [
     # Tasks
-    # path("", TaskListCreateView.as_view(), name="task-list-create"),
-    # path("<int:pk>/", TaskRetrieveUpdateDestroyView.as_view(), name="task-detail"),
-    # path("my-tasks/", MyTasksListView.as_view(), name="task-my-tasks"),
-    # path("<int:pk>/status/", TaskStatusUpdateView.as_view(), name="task-update-status"),
+    path("", TaskListCreateView.as_view(), name="task-list-create"),
+    path("<int:pk>/", TaskRetrieveUpdateDestroyView.as_view(), name="task-detail"),
+    path("my-tasks/", MyTasksListView.as_view(), name="task-my-tasks"),
+    path("<int:pk>/status/", TaskStatusUpdateView.as_view(), name="task-update-status"),
 
-    # # Assessments
-    # path("assessments/", AssessmentListCreateView.as_view(), name="assessment-list-create"),
-    # path("assessments/<int:pk>/", AssessmentRetrieveUpdateDestroyView.as_view(), name="assessment-detail"),
-    # path("assessments/worker/<int:worker_id>/", WorkerAssessmentsListView.as_view(), name="assessment-worker-list"),
+    # Assessments
+    path("assessments/", AssessmentListCreateView.as_view(), name="assessment-list-create"),
+    path("assessments/<int:pk>/", AssessmentRetrieveUpdateDestroyView.as_view(), name="assessment-detail"),
+    path("assessments/worker/<int:worker_id>/", WorkerAssessmentsListView.as_view(), name="assessment-worker-list"),
 ]
